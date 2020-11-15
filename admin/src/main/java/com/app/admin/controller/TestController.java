8b2a7e585b14eb57c26c32f060203b6d6011b87b
Daniil Roman <d.roman@just-ai.com>
2020-11-15 20:44:21 +0300

    working generated server

--- admin/src/main/java/com/app/admin/controller/TestController.java
+++ admin/src/main/java/com/app/admin/controller/TestController.java
@@ -1,7 +1,22 @@
 package com.app.admin.controller;
 
-import org.openapitools.api.ApiApi;
 
-public class TestController implements ApiApi {
+import com.app.admin.api.TestApi;
+import com.app.admin.api.model.TestItem;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+public class TestController implements TestApi {
+    @Override
+    public ResponseEntity<List<TestItem>> getTest(Long accountId) {
+        TestItem testItem1 = new TestItem();
+        testItem1.id(UUID.randomUUID()).setValues(Arrays.asList(1L, 2L, 3L));
+        return ResponseEntity.ok(Collections.singletonList(testItem1));
+    }
 }
