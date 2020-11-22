5702e3a0cc9101a4c94b45ba74653c35c98755f9
Daniil Roman <d.roman@just-ai.com>
2020-11-22 23:18:40 +0300

    working admin with analytics

--- admin/admin-be/src/main/java/com/app/admin/AdminApplication.java
+++ admin/admin-be/src/main/java/com/app/admin/AdminApplication.java
@@ -1,9 +1,12 @@
 package com.app.admin;
 
+import com.app.admin.config.AnalyticsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AnalyticsConfig.class)
 public class AdminApplication {
 
 	public static void main(String[] args) {
--- admin/admin-be/src/main/java/com/app/admin/controller/TestController.java
+++ admin/admin-be/src/main/java/com/app/admin/controller/TestController.java
@@ -2,27 +2,41 @@ package com.app.admin.controller;
 
 
 import com.app.admin.api.TestApi;
-//import com.app.analytics.api.client.TestApi;
+import com.app.admin.config.AnalyticsConfig;
+import com.app.analytics.api.ApiClient;
+import com.app.analytics.api.client.AnalyticsTestApi;
 import com.app.admin.api.model.TestItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
-import java.util.Collections;
+import javax.annotation.PostConstruct;
 import java.util.List;
-import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Controller
 public class TestController implements TestApi {
-//    @Autowired
-//    TestApi testApiClient;
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    AnalyticsConfig analyticsConfig;
+    private AnalyticsTestApi testApiClient;
+
+
+    @PostConstruct
+    public void setUp() {
+        ApiClient apiClient = new ApiClient(restTemplate);
+        apiClient.setBasePath(analyticsConfig.getUrl());
+        testApiClient = new AnalyticsTestApi(apiClient);
+    }
 
     @Override
     public ResponseEntity<List<TestItem>> getTest(Long accountId) {
-        TestItem testItem1 = new TestItem();
-//        testApiClient.getTest()
-        testItem1.id(UUID.randomUUID()).setValues(Arrays.asList(1L, 2L, 3L));
-        return ResponseEntity.ok(Collections.singletonList(testItem1));
+        return ResponseEntity.ok(
+                testApiClient.getTest(accountId).stream()
+                        .map(it -> new TestItem().id(it.getId()).values(it.getValues()))
+                        .collect(Collectors.toList())
+        );
     }
 }
