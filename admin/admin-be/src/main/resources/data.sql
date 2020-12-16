58eb44e40f21a32434eb0a6443f6ee9ddb2e6807
Splav555 <olegka35@gmail.com>
2020-12-17 01:08:13 +0300

    data sql

---
+++ admin/admin-be/src/main/resources/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO account VALUES ('25d9f318-e1cc-4630-8e19-11f2d8e7fbcd', 'Oleg', '$2a$10$a5P4q5DBpNNgzWQSADLoKuVYXUL93UsDjX.ntDVrfeTXs9VIUyQ5.' /* 12345 */, '2020-12-05', '2020-12-05');
+
+INSERT INTO cars VALUES ('c1a7e568-db8c-43b3-888b-2f8496824a00','Toyota','d2w','25d9f318-e1cc-4630-8e19-11f2d8e7fbcd');
+INSERT INTO cars VALUES ('c2ca2393-c2dd-46a3-b764-4dba15c09c62','Lada','Calina','25d9f318-e1cc-4630-8e19-11f2d8e7fbcd');
+INSERT INTO cars VALUES ('df3ca353-4a4f-4ecd-8a85-bda0add6bbc9','Test','test','25d9f318-e1cc-4630-8e19-11f2d8e7fbcd');
+
+INSERT INTO car_parameters VALUES ('c1a7e568-db8c-43b3-888b-2f8496824a00','1c1d293c-66e2-4e69-b005-9f7f28a0681a','160.0');
+INSERT INTO car_parameters VALUES ('c1a7e568-db8c-43b3-888b-2f8496824a00','347aae65-2db9-495a-ae4a-7f68b37f912e','silver');
+INSERT INTO car_parameters VALUES ('c1a7e568-db8c-43b3-888b-2f8496824a00','42c314a4-773b-449f-9df0-cce2e55bc95f','2015');
+INSERT INTO car_parameters VALUES ('c1a7e568-db8c-43b3-888b-2f8496824a00','4ba2c8dd-7706-4e10-afda-6358d7b712b0','100.0');
+INSERT INTO car_parameters VALUES ('c2ca2393-c2dd-46a3-b764-4dba15c09c62','1c1d293c-66e2-4e69-b005-9f7f28a0681a','90.0');
+INSERT INTO car_parameters VALUES ('c2ca2393-c2dd-46a3-b764-4dba15c09c62','347aae65-2db9-495a-ae4a-7f68b37f912e','black');
+INSERT INTO car_parameters VALUES ('c2ca2393-c2dd-46a3-b764-4dba15c09c62','42c314a4-773b-449f-9df0-cce2e55bc95f','2012');
+INSERT INTO car_parameters VALUES ('c2ca2393-c2dd-46a3-b764-4dba15c09c62','4ba2c8dd-7706-4e10-afda-6358d7b712b0','80.0');
+INSERT INTO car_parameters VALUES ('df3ca353-4a4f-4ecd-8a85-bda0add6bbc9','1c1d293c-66e2-4e69-b005-9f7f28a0681a','240.0');
+INSERT INTO car_parameters VALUES ('df3ca353-4a4f-4ecd-8a85-bda0add6bbc9','347aae65-2db9-495a-ae4a-7f68b37f912e','White');
+INSERT INTO car_parameters VALUES ('df3ca353-4a4f-4ecd-8a85-bda0add6bbc9','42c314a4-773b-449f-9df0-cce2e55bc95f','2017');
+INSERT INTO car_parameters VALUES ('df3ca353-4a4f-4ecd-8a85-bda0add6bbc9','4ba2c8dd-7706-4e10-afda-6358d7b712b0','280.0');
